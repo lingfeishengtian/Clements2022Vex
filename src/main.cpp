dfdf73ea1ea3a65b281bd9e629d56d62d15cb219
arianbeh <arianbehmard@gmail.com>
2021-09-23 16:07:02 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ void on_center_button() {
 	static bool pressed = false;
 	pressed = !pressed;
 	if (pressed) {
-		pros::lcd::set_text(2, "Don't touch me.");
+		pros::lcd::set_text(2, "pros is a pain.");
 	} else {
 		pros::lcd::clear_line(2);
 	}
@@ -76,19 +76,19 @@ void autonomous() {}
  */
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
+	pros::Motor left_mtr(5);
 	pros::Motor right_mtr(2);
 
 	while (true) {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
+		int left =master.get_analog(ANALOG_LEFT_Y);
 		int right = master.get_analog(ANALOG_RIGHT_Y);
 
 		left_mtr = left;
 		right_mtr = right;
 		
-		pros::delay(20);
+		pros::delay(500);
 	}
 }
